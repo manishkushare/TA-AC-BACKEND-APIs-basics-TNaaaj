ab04d161da884b2fb15130a7d37eaaa6f3d2d360
manishkushare <manishkushare@outlook.com>
2021-09-29 12:42:28 +0530

    book-api-v3

--- block-BNaaej/book-store/routes/v3Books.js
+++ block-BNaaej/book-store/routes/v3Books.js
@@ -27,7 +27,7 @@ router.post('/', async(req,res,next)=> {
     }
 });
 
-router.get('/listallcategories', async (req,res,next)=> {
+router.get('/categories', async (req,res,next)=> {
     try {
         const allCategories = await Books.distinct("categories");
         console.log(allCategories);
@@ -35,9 +35,80 @@ router.get('/listallcategories', async (req,res,next)=> {
     } catch (error) {
         next(error);
     }
+});
+router.get('/tags', async (req,res,next)=> {
+    try {
+        const allTags = await Books.distinct("tags");
+        console.log(allTags);
+        res.json({allTags});    
+    } catch (error) {
+        next(error);
+    }
+});
+router.get('/tags/ascending', async (req,res,next)=> {
+    try {
+        let allTags = await Books.distinct("tags");
+        console.log(allTags);
+        allTags = allTags.sort((a,b)=> a.localeCompare(b));
+        res.json({allTags});    
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.get('/tags/descending', async (req,res,next)=> {
+    try {
+        let allTags = await Books.distinct("tags");
+        console.log(allTags);
+        allTags = allTags.sort((a,b)=> b.localeCompare(a));
+        res.json({allTags});    
+    } catch (error) {
+        next(error);
+    }
+});
+router.get('/list-book-by-tags', async (req,res,next)=> {
+    const tag = req.query.tag;
+    try {
+        const books = await Books.find({tags : tag});
+        console.log(books);
+        res.json({books});
+    } catch (error) {
+        next(error);
+    }
+});
+router.get('/count-books-by-tag', async(req,res,next)=> {
+    const tag = req.query.tag;
+    try {
+        const bookCount = await Books.count({tags : tag});
+        console.log(bookCount);
+        res.json({category : bookCount});
+    } catch (error) {
+        next(error);
+    }
 })
 
+router.get('/list-books-by-category/', async (req,res,next)=> {
+    const category = req.query.category;
+    console.log(category);
+    try {
+        const books = await Books.find({categories : category});
+        console.log(books);
+        res.json({books});
+    } catch (error) {
+        next(error);
+    }
+});
 
+router.get('/count-books-by-category', async(req,res,next)=> {
+    const category = req.query.category;
+    try {
+        const bookCount = await Books.count({categories : category});
+        console.log(bookCount);
+        res.json({category : bookCount});
+    } catch (error) {
+        next(error);
+    }
+})
 
 router.get('/:id', async(req,res,next)=> {
     var id = req.params.id;
@@ -85,7 +156,7 @@ router.post('/:id/addComment', async(req,res,next)=> {
     }
 });
 
-// list all categories
+
 
 
 module.exports = router;
